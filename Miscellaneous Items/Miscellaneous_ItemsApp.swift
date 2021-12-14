28a727c0a22c74d46fa716fb6f218799f75fe640
Ryan Lim <rlim@bsd220.org>
2021-12-14 10:30:22 -0600

    Initial Commit

---
+++ Miscellaneous Items/Miscellaneous_ItemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Miscellaneous_ItemsApp.swift
+//  Miscellaneous Items
+//
+//  Created by Student on 12/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Miscellaneous_ItemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
